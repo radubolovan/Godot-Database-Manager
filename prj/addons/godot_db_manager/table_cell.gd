ca55f98bdddf093fb88b6941af5d29156a72cf0f
Radu Bolovan <radubolovan@gmail.com>
2020-03-16 20:10:12 +0200

    - Update todo.txt - TableCell: check for float type

--- prj/addons/godot_db_manager/table_cell.gd
+++ prj/addons/godot_db_manager/table_cell.gd
@@ -106,21 +106,44 @@ func on_button_pressed() -> void:
 
 # called when edit the data
 func on_text_changed(new_text : String) -> void :
+	if(new_text.empty()):
+		m_text = ""
+		$LineEdit.set_text(m_text)
+		$LineEdit.set_cursor_position(0)
+		return
+
 	if(m_prop_type == db_types.e_prop_type_int):
 		if(!check_integer(new_text)):
 			return
+
+	if(m_prop_type == db_types.e_prop_type_float):
+		if(!check_float(new_text)):
+			return
+
 	emit_signal("edit_data", m_prop_id, m_row_idx, new_text)
 
 # checks if the text is integer
 func check_integer(text : String) -> bool :
-	if(text.empty()):
-		m_text = text
-		$LineEdit.set_text(m_text)
-		$LineEdit.set_cursor_position(0)
-		return false
-
 	if(text.is_valid_integer()):
 		if(text.begins_with("0")):
+			m_text = "0"
+			$LineEdit.set_text(m_text)
+			$LineEdit.set_cursor_position(1)
+			return true
+
+		m_text = text
+		return true
+
+	$LineEdit.set_text(m_text)
+	$LineEdit.set_cursor_position(m_text.length())
+	return false
+
+func check_float(text : String) -> bool:
+	if(text.is_valid_float()):
+		if(text.begins_with("00") || text.begins_with("01") || text.begins_with("02")
+			 || text.begins_with("03") || text.begins_with("04") || text.begins_with("05")
+			 || text.begins_with("06") || text.begins_with("07") || text.begins_with("08")
+			 || text.begins_with("09")):
 			m_text = "0"
 			$LineEdit.set_text(m_text)
 			$LineEdit.set_cursor_position(1)
