e33582b5d29fc0191d8ab3bb035b5d3bbf4b41dc
Radu Bolovan <radubolovan@gmail.com>
2020-03-31 13:39:27 +0300

    Implement #10: "[IMPROVEMENT][2.0] Add a Cancel button to the New database dialog."

--- prj/addons/godot_db_manager/dlgs/new_db_dlg.gd
+++ prj/addons/godot_db_manager/dlgs/new_db_dlg.gd
@@ -12,19 +12,20 @@ signal create_new_db
 var m_current_db_name = ""
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$v_layout/buttons/ok_button.connect("pressed", self, "on_ok_btn_pressed")
+func _ready() -> void :
+	$v_layout/buttons/ok_btn.connect("pressed", self, "on_ok_btn_pressed")
+	$v_layout/buttons/cancel_btn.connect("pressed", self, "on_cancel_btn_pressed")
 	connect("about_to_show", self, "on_about_to_show")
 	$v_layout/db_info/db_edt.connect("text_changed", self, "on_text_changed")
 	$v_layout/db_info/db_edt.connect("text_entered", self, "on_text_confirmed")
 
 # Called when the node is about to be shown.
-func on_about_to_show() -> void:
+func on_about_to_show() -> void :
 	m_current_db_name = ""
 	$v_layout/db_info/db_edt.set_text(m_current_db_name)
 
 # called everytime the text is changed
-func on_text_changed(new_text: String) -> void:
+func on_text_changed(new_text: String) -> void :
 	var change_text = true
 	if(!gddb_constants.check_db_name(new_text)):
 		change_text = false
@@ -41,7 +42,7 @@ func on_text_changed(new_text: String) -> void:
 		$v_layout/db_info/db_edt.set_cursor_position(m_current_db_name.length())
 
 # called when the user presses the ENTER key
-func on_text_confirmed(text : String) -> void:
+func on_text_confirmed(text : String) -> void :
 	# print("GDDBNewDBDlg::on_text_confirmed(" + text + ")")
 	if(m_current_db_name.empty()):
 		return
@@ -49,10 +50,14 @@ func on_text_confirmed(text : String) -> void:
 	hide()
 
 # called when the OK button is pressed
-func on_ok_btn_pressed() -> void:
+func on_ok_btn_pressed() -> void :
 	if(!m_current_db_name.empty()):
 		handle_db_name()
 
+# called when the Cancel button is pressed
+func on_cancel_btn_pressed() -> void :
+	hide()
+
 # handles the name of the database
-func handle_db_name() -> void:
+func handle_db_name() -> void :
 	emit_signal("create_new_db", m_current_db_name)
